86164963549f47ccf062d7d39df61454595ad4d4
Jonathan <jfv213@gmail.com>
2019-10-12 16:03:37 -0400

    Add collision behaviour/freeze player axis

--- js/environment.js
+++ js/environment.js
@@ -1,7 +1,7 @@
 function Environment() {
 
   // 07c
-  //ELEMENT ONE (**LOOK UP MATERIAL OPTIONS**)
+  // ELEMENT ONE (**LOOK UP MATERIAL OPTIONS**)
   for (let i = 0; i < 100; i++) {
     let env2BlockGeometry = new THREE.BoxGeometry(1, 1, 1); //PRIMITIVE SHAPE AND SIZE
     let env2BlockMaterial = new THREE.MeshLambertMaterial({ color: 0x22CAC2 }); //COLOR OF MESH
@@ -11,4 +11,36 @@ function Environment() {
     env2Block.position.z = (Math.random() - 0.5) * 100;
     scene.add(env2Block); //DROP ELEMENT INTO VIRTUAL ENVIRONMENT
   }
+
+  let _vector = new THREE.Vector3(0, 0, 0)
+  for (let i = 0; i < 10; i++) {
+    let env3BlockGeometry = new THREE.CubeGeometry(1, 1, 1); //PRIMITIVE SHAPE AND SIZE
+    let env3BlockMaterial = new THREE.MeshLambertMaterial({ color: 0xff00C2 }); //COLOR OF MESH
+    let env3Block = new Physijs.BoxMesh(env3BlockGeometry, env3BlockMaterial); //MESH POINTS MAT TO GEOMETRY
+    env3Block.position.x = (Math.random() - 0.5) * 100;
+    env3Block.position.y = 5
+    env3Block.position.z = (Math.random() - 0.5) * 100;
+    scene.add(env3Block); //DROP ELEMENT INTO VIRTUAL ENVIRONMENT
+
+    // _vector.set(0, 0, 0);
+    env3Block.setAngularFactor(_vector);
+    env3Block.setAngularVelocity(_vector);
+    // env3Block.setLinearFactor(_vector);
+    // env3Block.setLinearVelocity(_vector);
+
+
+
+    env3Block.addEventListener('collision', function (other_object, linear_velocity, angular_velocity) {
+      console.log(other_object)
+      // console.log(linear_velocity)
+      // console.log(angular_velocity)
+      env3Block.material.wireframe = true
+      // env3Block.visible = false; // make any mesh disappear on collision...
+    });
+
+  }
+
+
+  
+
 }
--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 // 201 
-Physijs.scripts.worker = '/js/physijs_worker.js';
-Physijs.scripts.ammo = '/js/ammo.js';
+Physijs.scripts.worker = './lib/physijs_worker.js';
+Physijs.scripts.ammo = './lib/ammo.js';
 
 
 // 02
 //RENDERER INPUT, SCENE (virtual environment)/CAMERA 
-let scene = new THREE.Scene();
+// let scene = new THREE.Scene();
+let scene = new Physijs.Scene;
+// let scene = new Physijs.Scene({ reportsize: 50, fixedTimeStep: 1 / 20 }); //Slow down scene to fix rotation bug
+scene.setGravity(new THREE.Vector3( 0, -9.8, 0));
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // camera.position.z = 5;
@@ -44,14 +47,33 @@ window.addEventListener('resize', () => {
 let raycaster = new THREE.Raycaster();
 let mouse = new THREE.Vector2();
 
+
+//GROUND
+let groundGeometry = new THREE.PlaneGeometry(7000, 3000, 0); //PRIMITIVE SHAPE AND SIZE
+let groundMaterial = new THREE.MeshBasicMaterial({ color: 0x22CAC2 }); //COLOR OF MESH
+// let ground = new THREE.Mesh(groundGeometry, groundMaterial); //MESH POINTS MAT TO GEOMETRY
+let ground = new Physijs.PlaneMesh(groundGeometry, groundMaterial); //MESH POINTS MAT TO GEOMETRY
+ground.rotation.x = -0.5 * Math.PI;
+ground.name = 'ground'
+scene.add(ground); //DROP ELEMENT INTO VIRTUAL ENVIRONMENT
+
+
+
+
+
 // 07
 //ELEMENT ONE (**LOOK UP MATERIAL OPTIONS**)
-let playerGeometry = new THREE.BoxGeometry(1, 1, 1); //PRIMITIVE SHAPE AND SIZE
+let playerGeometry = new THREE.CubeGeometry(1, 1, 1, 0); //PRIMITIVE SHAPE AND SIZE
 let playerMaterial = new THREE.MeshLambertMaterial({ color: 0x22CAC2 }); //COLOR OF MESH
-let player = new THREE.Mesh(playerGeometry, playerMaterial); //MESH POINTS MAT TO GEOMETRY
+// let player = new THREE.Mesh(playerGeometry, playerMaterial); //MESH POINTS MAT TO GEOMETRY
+let player = new Physijs.BoxMesh(playerGeometry, playerMaterial); //MESH POINTS MAT TO GEOMETRY
+player.position.set(0, 1, 0);
+player.name = 'player';
 scene.add(player); //DROP ELEMENT INTO VIRTUAL ENVIRONMENT
 camera.position.set(0, 2, 5);
 player.add(camera)
+
+
 
 
 // 08
@@ -70,13 +92,24 @@ let light3 = new THREE.PointLight(0xFFFFFF, 2, 2);
 light3.position.set(0, 0, 25)
 player.add(light3)
 
-
 // 09
 //RENDER LOOP
 // 102
 //Normalize animation loop
 let lastTimeStamp = 0;
+// player.__dirtyPosition = true;
+// player.__dirtyRotation = true;
+
+let _vector = new THREE.Vector3(0, 0, 0)
 let animate = function (timeStamp) {
+  // player.__dirtyPosition = true;
+  // player.__dirtyRotation = true;
+  
+  player.setAngularFactor(_vector);
+  player.setAngularVelocity(_vector);
+  player.setLinearVelocity(new THREE.Vector3(0, 0, 0));
+
+
   requestAnimationFrame(animate);
 
   let timeDelta = (timeStamp - lastTimeStamp)/1000;
@@ -95,42 +128,59 @@ let animate = function (timeStamp) {
   
   //LEFT
   if (input.isLeftPressed) {
+    player.__dirtyPosition = true;
+    player.__dirtyRotation = true;
     player.position.x -= Math.sin(player.rotation.y + Math.PI / 2) * playerSpeed;
     player.position.z -= Math.cos(player.rotation.y + Math.PI / 2) * playerSpeed;
   }
   //RIGHT
   if (input.isRightPressed) {
+    player.__dirtyPosition = true;
+    player.__dirtyRotation = true;
     player.position.x += Math.sin(player.rotation.y + Math.PI / 2) * playerSpeed;
     player.position.z += Math.cos(player.rotation.y + Math.PI / 2) * playerSpeed;
   }
   //JUMP  
   if (input.isSpacePressed) {
+    player.__dirtyPosition = true;
+    player.__dirtyRotation = true;
     player.position.y += playerSpeed*2;
   }
   //FWD 
   if (input.isFwdPressed) {
+    player.__dirtyPosition = true;
+    player.__dirtyRotation = true;
     player.position.x -= Math.sin(player.rotation.y) * playerSpeed;
     player.position.z -= Math.cos(player.rotation.y) * playerSpeed;
   }
   //BACK 
   if (input.isBwdPressed) {
+    player.__dirtyPosition = true;
+    player.__dirtyRotation = true;
     player.position.x += Math.sin(player.rotation.y) * playerSpeed;
     player.position.z += Math.cos(player.rotation.y) * playerSpeed;
   }
   //RotLeft
   if (input.isRLPressed) {
-    player.rotation.y += playerSpeed/4;
+    // player.rotation.y += playerSpeed/4;
+    player.rotation.y += .1;
+    player.__dirtyPosition = true;
+    player.__dirtyRotation = true;
   }
   //RotRight
   if (input.isRRPressed) {
+
     player.rotation.y -= playerSpeed/4;
+    player.__dirtyPosition = true;
+    player.__dirtyRotation = true;
   }
 
   //GRAVITY...fix this please
-  if (player.position.y >= 0) {
+  if (player.position.y >= 1) {
     player.position.y -= 0.2 * boost;
   }
   // camera.lookAt(player.position)
+  scene.simulate();
   renderer.render(scene, camera);
 };
 
