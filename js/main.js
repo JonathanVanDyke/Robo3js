cd3ddc8689bbd87592eafaf841bc3e9d14407c56
Jonathan <jfv213@gmail.com>
2019-10-13 10:38:53 -0400

    Refactor into init function

--- js/main.js
+++ js/main.js
@@ -1,102 +1,100 @@
+let camera, renderer, scene, player, bullets, input, environment, _vector, clock, lastTimeStamp;
 
 
-// 201 
-Physijs.scripts.worker = './lib/physijs_worker.js';
-Physijs.scripts.ammo = './lib/ammo.js';
+function init() {
+  // 201 
+  Physijs.scripts.worker = './lib/physijs_worker.js';
+  Physijs.scripts.ammo = './lib/ammo.js';
 
+  // 02
+  //RENDERER INPUT, SCENE (virtual environment)/CAMERA 
+  // let scene = new THREE.Scene();
+  scene = new Physijs.Scene;
 
-// 02
-//RENDERER INPUT, SCENE (virtual environment)/CAMERA 
-// let scene = new THREE.Scene();
-let scene = new Physijs.Scene;
+  // let scene = new Physijs.Scene({ reportsize: 50, fixedTimeStep: 1 / 20 }); //Slow down scene to fix rotation bug
+  scene.setGravity(new THREE.Vector3(0, -9.8, 0));
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+  //202
+  //Bullets
+  bullets = new Bullets();
 
-// let scene = new Physijs.Scene({ reportsize: 50, fixedTimeStep: 1 / 20 }); //Slow down scene to fix rotation bug
-scene.setGravity(new THREE.Vector3( 0, -9.8, 0));
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  // 101
+  //INPUT OBJECT
+  input = new Input();
 
-//MECH
+  // 001
+  // Environment
+  environment = new Environment();
 
-//202
-//Bullets
-let bullets = new Bullets();
-
-// 101
-//INPUT OBJECT
-let input = new Input();
-
-
-
-// 001
-// Environment
-let environment = new Environment();
-
-// 03
-//INSTANCE OF RENDERER
-let renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor("#e5e5e5"); //BACKGROUND COLOR
-
-// 04
-//ADD CANVAS ELEMENT TO DOM
-document.body.appendChild(renderer.domElement);
-
-// 05
-//MAKE WINDOW RESPONSIVE ON RESIZE
-window.addEventListener('resize', () => {
+  // 03
+  //INSTANCE OF RENDERER
+  renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.aspect = window.innerWidth / window.innerHeight;
+  renderer.setClearColor("#e5e5e5"); //BACKGROUND COLOR
 
-  camera.updateProjectionMatrix();
-})
+  // 04
+  //ADD CANVAS ELEMENT TO DOM
+  document.body.appendChild(renderer.domElement);
 
-// 06
-//RAYCASTER => VECTOR 'RAY'... RAY === Array? (like vector array?)
-let raycaster = new THREE.Raycaster();
-let mouse = new THREE.Vector2();
+  // 05
+  //MAKE WINDOW RESPONSIVE ON RESIZE
+  window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+  })
+
+  // 06
+  //RAYCASTER => VECTOR 'RAY'... RAY === Array? (like vector array?)
+  let raycaster = new THREE.Raycaster();
+  let mouse = new THREE.Vector2();
+
+  // 07
+  //ELEMENT ONE (**LOOK UP MATERIAL OPTIONS**)
+  let playerGeometry = new THREE.CubeGeometry(5, 8, 5, 100); //PRIMITIVE SHAPE AND SIZE (set 3rd val to 111 for cat paw)
+  let playerMaterial = new THREE.MeshLambertMaterial({ color: 0x22CAC2, transparent: true, opacity: .25 }); //COLOR OF MESH
+  //ELEMENT ONE (**LOOK UP MATERIAL OPTIONS**)
+
+  // //Cat mode
+  // let playerGeometry = new THREE.CubeGeometry(1, 2, 111, 100); //PRIMITIVE SHAPE AND SIZE (set 3rd val to 111 for cat paw)
+  // let playerMaterial = new THREE.MeshLambertMaterial({ color: 0x22CAC2, transparent: true, opacity: 1.0 }); //COLOR OF MESH
+  // //Cat mode
+
+  // let player = new THREE.Mesh(playerGeometry, playerMaterial); //MESH POINTS MAT TO GEOMETRY
+  player = new Physijs.BoxMesh(playerGeometry, playerMaterial); //MESH POINTS MAT TO GEOMETRY
+  player.position.set(0, 5, 0);
+  player.name = 'player';
+  camera.position.set(0, 6, 10);
+  camera.rotation.x = -.2
+  player.add(camera)
+
+  // 08
+  //LIGHT ONE
+  let light1 = new THREE.PointLight(0xFFFFFF, 2, 1000);
+  light1.position.set(0, 80, 25)
+  scene.add(light1)
+
+  //LIGHT TWO
+  let light2 = new THREE.AmbientLight(0xFFFFFF, .25, 2);
+  light2.position.set(0, 0, 25)
+  scene.add(light2)
+
+  // 09
+  //RENDER LOOP
+  // 102
+  //Normalize animation loop
+  lastTimeStamp = 0;
+
+  clock = new THREE.Clock();
+  _vector = new THREE.Vector3(0, 0, 0)
+
+}
 
 
 
-// 07
-//ELEMENT ONE (**LOOK UP MATERIAL OPTIONS**)
-let playerGeometry = new THREE.CubeGeometry(5, 8, 5, 100); //PRIMITIVE SHAPE AND SIZE (set 3rd val to 111 for cat paw)
-let playerMaterial = new THREE.MeshLambertMaterial({ color: 0x22CAC2, transparent: true, opacity: .25 }); //COLOR OF MESH
-//ELEMENT ONE (**LOOK UP MATERIAL OPTIONS**)
 
-// //Cat mode
-// let playerGeometry = new THREE.CubeGeometry(1, 2, 111, 100); //PRIMITIVE SHAPE AND SIZE (set 3rd val to 111 for cat paw)
-// let playerMaterial = new THREE.MeshLambertMaterial({ color: 0x22CAC2, transparent: true, opacity: 1.0 }); //COLOR OF MESH
-// //Cat mode
-
-// let player = new THREE.Mesh(playerGeometry, playerMaterial); //MESH POINTS MAT TO GEOMETRY
-let player = new Physijs.BoxMesh(playerGeometry, playerMaterial); //MESH POINTS MAT TO GEOMETRY
-player.position.set(0, 5, 0);
-player.name = 'player';
-camera.position.set(0, 6, 10);
-camera.rotation.x = -.2
-player.add(camera)
-
-
-
-// 08
-//LIGHT ONE
-let light1 = new THREE.PointLight(0xFFFFFF, 2, 1000);
-light1.position.set(0, 80, 25)
-scene.add(light1)
-
-//LIGHT TWO
-let light2 = new THREE.AmbientLight(0xFFFFFF, .25, 2);
-light2.position.set(0, 0, 25)
-scene.add(light2)
-
-// 09
-//RENDER LOOP
-// 102
-//Normalize animation loop
-let lastTimeStamp = 0;
-
-let clock = new THREE.Clock();
-let _vector = new THREE.Vector3(0, 0, 0)
 let animate = function (timeStamp) {
   // player.__dirtyPosition = true;
   // player.__dirtyRotation = true;
@@ -209,6 +207,8 @@ let animate = function (timeStamp) {
   scene.simulate();
   renderer.render(scene, camera);
 };
+
+init();
 
 // 11 
 //...10 is mouse event listener, 12 is adding listener to window)...
